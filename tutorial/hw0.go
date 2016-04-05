b52e39b5c084d2c78320753716256a80465d9750
luckyharryji <xiangyu.ji@outlook.com>
2016-04-05 00:05:26 -0500

    naive method to resolve data race

--- tutorial/hw0.go
+++ tutorial/hw0.go
@@ -10,65 +10,44 @@ import (
 )
 
 var idAssignmentChan = make(chan string)
-// var client_id_to_stream = make(map[string] net.Conn)
-var clientIdToChannel = make(map[string] chan string)
-var clientIdToChannelTest = make(map[string] chan string)
+var clientIdToStream = make(map[string] net.Conn)
+var contentQueue = make(chan string)
 
-var idQueue = make(chan string)
-
-
-func Write(conn net.Conn, id string) {
-	for content := range clientIdToChannel[id]{
-        conn.Write([]byte(string(content)))
-	}
+func SendToQueue(content string) {
+    contentQueue <- content
 }
-
-func WriteToAll(content string){
-    for id := range clientIdToChannel {
-        clientIdToChannel[id] <- content
-    }
-}
-
-func TalkToSingle(id string, content string) {
-    if channel_of_id, exist := clientIdToChannel[id]; exist {
-        // fmt.Println(content + " " + id, " is talking to single")
-        channel_of_id <- content
-        // fmt.Println(content + " " + id, " send to single")
-    }
-}
-
-func PutIdToQueue(client_id string) {
-    idQueue <- client_id
-}
-
 
 func ParseContent(content string, client_id string) {
-    fmt.Println(content, " is parsing")
     split_content := strings.Split(content, ":")
     command := strings.Trim(split_content[0], " ")
     contentInfo := strings.Trim(strings.Join(split_content[1:], ":"), " ")
     switch command {
     case "whoami":
-        TalkToSingle(client_id, "chitter: " + client_id + "\r\n")
+        SendToQueue(client_id + ": chitter: " + client_id + "\r\n")
     case "all":
-        WriteToAll(client_id + ": " + contentInfo)
+        SendToQueue("all: " + client_id + ": " + contentInfo)
     default:
-        TalkToSingle(command, client_id + ": " + contentInfo)
+        SendToQueue(command + ": " + client_id + ": " + contentInfo)
     }
 }
 
-func HandleConnection(conn net.Conn) {
+func SendContent(content string) {
+    split_content := strings.Split(content, ":")
+    command := strings.Trim(split_content[0], " ")
+    contentInfo := strings.Trim(strings.Join(split_content[1:], ":"), " ")
+    switch command {
+    case "all":
+        for id := range clientIdToStream {
+            clientIdToStream[id].Write([]byte(string(contentInfo)))
+        }
+    default:
+        clientIdToStream[command].Write([]byte(string(contentInfo)))
+    }
+}
+
+
+func HandleConnectionWithId(conn net.Conn, client_id string) {
     b := bufio.NewReader(conn)
-    client_id := <- idAssignmentChan
-
-    // idQueue <- client_id
-    // PutIdToQueue(client_id)
-
-    // reqrite to assign channel in singel thread
-    var channelForId = make(chan string)
-    clientIdToChannel[client_id] = channelForId
-
-    go Write(conn, client_id)
     for {
         line, err := b.ReadBytes('\n')
         if err != nil {
@@ -86,29 +65,15 @@ func IdManager() {
     }
 }
 
-// func CreateChannelForId(){
-//     for id:= range idQueue{
-//
-//         // id := <- idQueue
-//
-//         fmt.Println(id, " is assigning")
-//         var channelForId = make(chan string)
-//         channelForId <- (id + " is ready")
-//         clientIdToChannelTest[id] = channelForId
-//     }
-// }
 
-func CreateChannelForId(){
+func SteamListener() {
     for {
-        var channelForId = make(chan string)
         select {
-        case id := <- idQueue:
-            fmt.Println(id, " is assigning")
-            clientIdToChannelTest[id] = channelForId
+        case content := <- contentQueue:
+            SendContent(content)
         }
     }
 }
-
 
 func main() {
     if len(os.Args) < 2{
@@ -122,13 +87,15 @@ func main() {
         fmt.Fprintln(os.Stderr, "Can't connect to port")
         os.Exit(1)
     }
-    go IdManager()
 
-    // go CreateChannelForId()
+    go IdManager()
+    go SteamListener()
 
     fmt.Println("Listening on port", os.Args[1])
     for{
         conn, _ := server.Accept()
-        go HandleConnection(conn)
+        client_id := <- idAssignmentChan
+        clientIdToStream[client_id] = conn
+        go HandleConnectionWithId(conn, client_id)
     }
 }
